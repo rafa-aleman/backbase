2206fc85b826230756d2dc337d21c79bf574badb
rafa-aleman <rafael.aleman@gmail.com>
2017-11-30 19:43:05 +0100

    Skeleton application

---
+++ application-web/src/main/java/com/backbase/application/WebApplication.java
@@ -0,0 +1,22 @@
+package com.backbase.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * WebApplication
+
+ * @author Rafael Aleman
+ */
+@SpringBootApplication
+public class WebApplication {
+
+    /**
+     * Main method that runs Spring Boot.
+     *
+     * @param args the usual command line arguments
+     */
+    public static void main(final String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
